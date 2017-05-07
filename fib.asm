e90bacb64576812bb9b8d81c97648a5afe1909f8
Evgenius2020 <spleen47@mail.ru>
2017-05-07 23:21:49 +0700

    Fib.asm complete code

--- fib.asm
+++ fib.asm
@@ -1,4 +1,66 @@
-set 0
-set 8
-add 7
-jmp 0
+//  fib(n) {
+//  	n -=2;    
+//  	res =1;
+//  	prev = 1;
+//  	while(n > 0) {
+//		    n--;
+//		    buf = res;
+//		    res = prev;
+//		    prev = buf;
+//		    res += prev;
+//      }
+//	    return res;
+// }
+
+// Commands code binding
+.cmd add 1
+.cmd sub 2
+.cmd load 3
+.cmd store 4
+.cmd set 5
+.cmd jmp 6
+.cmd jz 7
+
+// Variable difinition
+.variables res, prev, n, buf;
+
+// Code
+set 2
+store buf
+load n
+sub buf
+store n
+
+set 1
+store res
+
+set 1
+store prev
+
+.cycle
+load n
+jz exit
+
+set 1
+store buf
+load n
+sub buf
+store n
+
+load res
+store buf
+
+load prev
+store res
+
+load buf
+store prev
+
+add res
+store res
+
+jmp cycle
+
+.exit
+load res
+jmp exit
